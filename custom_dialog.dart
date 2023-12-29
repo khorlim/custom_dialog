da5c603158af3627699247dc77e11802e6507113
khorlim <khorlim2001@gmail.com>
2023-12-29 12:36:52 +0800

    update the way to get keyboard height

--- custom_dialog.dart
+++ custom_dialog.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:tunaipro/share_code/keyboard_size_provider/keyboard_size_provider.dart';
 import 'dart:ui' as ui;
 import 'triangle.dart';
 
@@ -50,13 +51,11 @@ class CustomDialog extends StatefulWidget {
   })  : dialogHeight = height,
         screenHeight = MediaQuery.of(context).size.height,
         screenWidth = MediaQuery.of(context).size.width,
-        safeAreaTopHeight = MediaQueryData.fromView(ui.window).padding.top,
-        paddingWhenKeyboardShow = MediaQuery.of(context).viewInsets.bottom;
+        safeAreaTopHeight = MediaQueryData.fromView(ui.window).padding.top;
 
   final double screenHeight;
   final double screenWidth;
   final double safeAreaTopHeight;
-  final double paddingWhenKeyboardShow;
 
   double dialogHeight;
 
@@ -307,109 +306,116 @@ class _CustomDialogState extends State<CustomDialog> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: KeyboardVisibilityBuilder(builder: (context, isKeyboardVisible) {
-        return LayoutBuilder(builder: (context, constraints) {
-          if (isKeyboardVisible) {
-            double screenHeight = constraints.maxHeight;
+    return KeyboardSizeProvider(
+      child: SafeArea(
+        child: Consumer<ScreenHeight>(builder: (context, screenHeight, child) {
+          bool isKeyboardVisible = screenHeight.isOpen;
+          double paddingWhenKeyboardShow = screenHeight.keyboardHeight;
+          return LayoutBuilder(builder: (context, constraints) {
+            if (isKeyboardVisible) {
+              double screenHeight = constraints.maxHeight;
 
-            double newHeight = widget.height -
-                (widget.paddingWhenKeyboardShow -
-                    (screenHeight - (widget.height + dialogTopPos!)));
+              double newHeight = widget.height -
+                  (paddingWhenKeyboardShow -
+                      (screenHeight - (widget.height + dialogTopPos!)));
 
-            if (widget.pushDialogAboveWhenKeyboardShow) {
-              newHeight = widget.height -
-                  (widget.paddingWhenKeyboardShow -
-                      (screenHeight - (widget.height + 10)));
-              enableArrow = false;
+              if (widget.pushDialogAboveWhenKeyboardShow) {
+                newHeight = widget.height -
+                    (paddingWhenKeyboardShow -
+                        (screenHeight - (widget.height + 10)));
+                enableArrow = false;
+              }
+
+              widget.dialogHeight =
+                  newHeight > 0 ? newHeight - 5 : widget.height;
+            } else {
+              if (widget.pushDialogAboveWhenKeyboardShow) {
+                Future.delayed(Duration(milliseconds: 200), () {
+                  enableArrow =
+                      widget.targetWidgetContext != null ? true : false;
+                });
+              }
+              widget.dialogHeight = widget.height;
             }
-
-            widget.dialogHeight = newHeight > 0 ? newHeight - 5 : widget.height;
-          } else {
-            if (widget.pushDialogAboveWhenKeyboardShow) {
-              Future.delayed(Duration(milliseconds: 200), () {
-                enableArrow = widget.targetWidgetContext != null ? true : false;
-              });
-            }
-            widget.dialogHeight = widget.height;
-          }
-          return Stack(
-            alignment: Alignment.center,
-            children: [
-              GestureDetector(
-                onTap: widget.onTapOutside != null
-                    ? () => widget.onTapOutside?.call()
-                    : () {
-                        Navigator.pop(context);
-                      },
-              ),
-              AnimatedPositioned(
-                duration: Duration(milliseconds: 300),
-                top: isKeyboardVisible && widget.pushDialogAboveWhenKeyboardShow
-                    ? 10
-                    : dialogTopPos,
-                left: dialogLeftPos,
-                child: Material(
-                  clipBehavior: Clip.antiAlias,
-                  elevation: 5,
-                  shadowColor: Colors.grey.withOpacity(0.3),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Container(
-                    clipBehavior: Clip.antiAlias,
-                    padding: EdgeInsets.zero,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(10),
-                            bottomRight: Radius.circular(10)),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.1),
-                            spreadRadius: 2,
-                            blurRadius: 10,
-                            offset: Offset(0, -2),
-                          ),
-                        ]),
-                    height: widget.dialogHeight,
-                    width: widget.width,
-                    child: Column(
-                      children: [
-                        widget.appBar ?? Container(),
-                        Expanded(child: widget.child),
-                      ],
-                    ),
-                  ),
+            return Stack(
+              alignment: Alignment.center,
+              children: [
+                GestureDetector(
+                  onTap: widget.onTapOutside != null
+                      ? () => widget.onTapOutside?.call()
+                      : () {
+                          Navigator.pop(context);
+                        },
                 ),
-              ),
-              if (enableArrow)
-                Positioned(
-                  top: arrowTopPos,
-                  left: arrowLeftPos,
-                  child: PhysicalModel(
-                    color: Colors.transparent,
-                    elevation: 3,
-                    shadowColor: Colors.grey.withOpacity(0.06),
-                    shape: BoxShape.circle,
-                    child: CustomPaint(
-                      painter: _getArrowPainter(arrowPointing),
-                      child: Container(
-                        width: arrowPointing == ArrowPointing.top ||
-                                arrowPointing == ArrowPointing.bottom
-                            ? widget.arrowWidth
-                            : widget.arrowHeight,
-                        height: arrowPointing == ArrowPointing.top ||
-                                arrowPointing == ArrowPointing.bottom
-                            ? widget.arrowHeight
-                            : widget.arrowWidth,
+                AnimatedPositioned(
+                  duration: Duration(milliseconds: 300),
+                  top: isKeyboardVisible &&
+                          widget.pushDialogAboveWhenKeyboardShow
+                      ? 10
+                      : dialogTopPos,
+                  left: dialogLeftPos,
+                  child: Material(
+                    clipBehavior: Clip.antiAlias,
+                    elevation: 5,
+                    shadowColor: Colors.grey.withOpacity(0.3),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Container(
+                      clipBehavior: Clip.antiAlias,
+                      padding: EdgeInsets.zero,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10)),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.1),
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                              offset: Offset(0, -2),
+                            ),
+                          ]),
+                      height: widget.dialogHeight,
+                      width: widget.width,
+                      child: Column(
+                        children: [
+                          widget.appBar ?? Container(),
+                          Expanded(child: widget.child),
+                        ],
                       ),
                     ),
                   ),
-                )
-            ],
-          );
-        });
-      }),
+                ),
+                if (enableArrow)
+                  Positioned(
+                    top: arrowTopPos,
+                    left: arrowLeftPos,
+                    child: PhysicalModel(
+                      color: Colors.transparent,
+                      elevation: 3,
+                      shadowColor: Colors.grey.withOpacity(0.06),
+                      shape: BoxShape.circle,
+                      child: CustomPaint(
+                        painter: _getArrowPainter(arrowPointing),
+                        child: Container(
+                          width: arrowPointing == ArrowPointing.top ||
+                                  arrowPointing == ArrowPointing.bottom
+                              ? widget.arrowWidth
+                              : widget.arrowHeight,
+                          height: arrowPointing == ArrowPointing.top ||
+                                  arrowPointing == ArrowPointing.bottom
+                              ? widget.arrowHeight
+                              : widget.arrowWidth,
+                        ),
+                      ),
+                    ),
+                  )
+              ],
+            );
+          });
+        }),
+      ),
     );
   }
 
