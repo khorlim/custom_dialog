e54208de4947f7b16c1e5c5decce09e581fb2143
khorlim <khorlim2001@gmail.com>
2024-01-14 00:18:20 +0800

    hehe

---
+++ utils/custom_modal_bottom_sheet.dart
@@ -0,0 +1,175 @@
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:tunaipro/share_code/custom_dialog/utils/custom_route.dart';
+
+const Duration _bottomSheetEnterDuration = Duration(milliseconds: 250);
+const Duration _bottomSheetExitDuration = Duration(milliseconds: 200);
+const Curve _modalBottomSheetCurve = decelerateEasing;
+const double _minFlingVelocity = 700.0;
+const double _closeProgressThreshold = 0.5;
+const double _defaultScrollControlDisabledMaxHeightRatio = 9.0 / 16.0;
+
+class CustomModalBottomSheet<T> extends StatefulWidget {
+  const CustomModalBottomSheet({
+    super.key,
+    required this.route,
+    this.backgroundColor,
+    this.elevation,
+    this.shape,
+    this.clipBehavior,
+    this.constraints,
+    this.isScrollControlled = false,
+    this.scrollControlDisabledMaxHeightRatio =
+        _defaultScrollControlDisabledMaxHeightRatio,
+    this.enableDrag = true,
+    this.showDragHandle = false,
+  });
+
+  final CustomPageRoute<T> route;
+  final bool isScrollControlled;
+  final double scrollControlDisabledMaxHeightRatio;
+  final Color? backgroundColor;
+  final double? elevation;
+  final ShapeBorder? shape;
+  final Clip? clipBehavior;
+  final BoxConstraints? constraints;
+  final bool enableDrag;
+  final bool showDragHandle;
+
+  @override
+  _ModalBottomSheetState<T> createState() => _ModalBottomSheetState<T>();
+}
+
+class _ModalBottomSheetState<T> extends State<CustomModalBottomSheet<T>> {
+  ParametricCurve<double> animationCurve = _modalBottomSheetCurve;
+
+  String _getRouteLabel(MaterialLocalizations localizations) {
+    switch (Theme.of(context).platform) {
+      case TargetPlatform.iOS:
+      case TargetPlatform.macOS:
+        return '';
+      case TargetPlatform.android:
+      case TargetPlatform.fuchsia:
+      case TargetPlatform.linux:
+      case TargetPlatform.windows:
+        return localizations.dialogLabel;
+    }
+  }
+
+  EdgeInsets _getNewClipDetails(Size topLayerSize) {
+    return EdgeInsets.fromLTRB(0, 0, 0, topLayerSize.height);
+  }
+
+  void handleDragStart(DragStartDetails details) {
+    // Allow the bottom sheet to track the user's finger accurately.
+    animationCurve = Curves.linear;
+  }
+
+  void handleDragEnd(DragEndDetails details, {bool? isClosing}) {
+    // Allow the bottom sheet to animate smoothly from its current position.
+    animationCurve = _BottomSheetSuspendedCurve(
+      widget.route.animation!.value,
+      curve: _modalBottomSheetCurve,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    assert(debugCheckHasMediaQuery(context));
+    assert(debugCheckHasMaterialLocalizations(context));
+    final MaterialLocalizations localizations =
+        MaterialLocalizations.of(context);
+    final String routeLabel = _getRouteLabel(localizations);
+
+    return AnimatedBuilder(
+      animation: widget.route.animation!,
+      child: BottomSheet(
+        // animationController: widget.route._animationController,
+        onClosing: () {
+          if (widget.route.isCurrent) {
+            Navigator.pop(context);
+          }
+        },
+        builder: widget.route.builder,
+        backgroundColor: widget.backgroundColor,
+        elevation: widget.elevation,
+        shape: widget.shape,
+        clipBehavior: widget.clipBehavior,
+        constraints: widget.constraints,
+        enableDrag: widget.enableDrag,
+        showDragHandle: widget.showDragHandle,
+        onDragStart: handleDragStart,
+        onDragEnd: handleDragEnd,
+      ),
+      builder: (BuildContext context, Widget? child) {
+        final double animationValue = animationCurve.transform(
+          widget.route.animation!.value,
+        );
+        return SizedBox(
+          child: Semantics(
+            scopesRoute: true,
+            namesRoute: true,
+            label: routeLabel,
+            explicitChildNodes: true,
+            child: ClipRect(child: child
+
+                // _BottomSheetLayoutWithSizeListener(
+                //   onChildSizeChanged: (Size size) {
+                //     // widget.route._didChangeBarrierSemanticsClip(
+                //     //   _getNewClipDetails(size),
+                //     // );
+                //   },
+                //   animationValue: animationValue,
+                //   isScrollControlled: widget.isScrollControlled,
+                //   scrollControlDisabledMaxHeightRatio:
+                //       widget.scrollControlDisabledMaxHeightRatio,
+                //   child: child,
+                // ),
+                ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _BottomSheetSuspendedCurve extends ParametricCurve<double> {
+  /// Creates a suspended curve.
+  const _BottomSheetSuspendedCurve(
+    this.startingPoint, {
+    this.curve = Curves.easeOutCubic,
+  });
+
+  /// The progress value at which [curve] should begin.
+  final double startingPoint;
+
+  /// The curve to use when [startingPoint] is reached.
+  ///
+  /// This defaults to [Curves.easeOutCubic].
+  final Curve curve;
+
+  @override
+  double transform(double t) {
+    assert(t >= 0.0 && t <= 1.0);
+    assert(startingPoint >= 0.0 && startingPoint <= 1.0);
+
+    if (t < startingPoint) {
+      return t;
+    }
+
+    if (t == 1.0) {
+      return t;
+    }
+
+    final double curveProgress = (t - startingPoint) / (1 - startingPoint);
+    final double transformed = curve.transform(curveProgress);
+    return lerpDouble(startingPoint, 1, transformed)!;
+  }
+
+  @override
+  String toString() {
+    return '${describeIdentity(this)}($startingPoint, $curve)';
+  }
+}
---
+++ utils/custom_route.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tunaipro/share_code/custom_dialog/custom_dialog.dart';
+import 'package:tunaipro/share_code/custom_dialog/utils/custom_modal_bottom_sheet.dart';
+import 'package:tunaipro/share_code/function/dialog_manager.dart';
+import 'package:tunaipro/share_code/responsive/device_type.dart';
+
+class CustomPageRoute<T> extends PopupRoute<T> {
+  final WidgetBuilder builder;
+  final DialogType dialogType;
+  final double? width;
+
+  final double? height;
+
+  final AlignTargetWidget? alignTargetWidget;
+
+  final bool? enableArrow;
+
+  final double? arrowWidth;
+
+  final double? arrowHeight;
+
+  final BuildContext? targetWidgetContext;
+
+  final Function? onTapOutside;
+
+  final Offset? adjustment;
+
+  final bool? showOverFlowArrow;
+
+  final bool? jumpWhenOverflow;
+
+  final double? overflowLeft;
+
+  final double? bottomSheetHeight;
+
+  final bool? pushDialogAboveWhenKeyboardShow;
+
+  final bool? followArrow;
+
+  final double? distanceBetweenTargetWidget;
+
+  CustomPageRoute({
+    required this.builder,
+    this.dialogType = DialogType.adaptivePosition,
+    this.width,
+    this.height,
+    this.alignTargetWidget,
+    this.enableArrow,
+    this.arrowWidth,
+    this.arrowHeight,
+    this.targetWidgetContext,
+    this.onTapOutside,
+    this.adjustment,
+    this.showOverFlowArrow,
+    this.jumpWhenOverflow,
+    this.overflowLeft,
+    this.bottomSheetHeight,
+    this.pushDialogAboveWhenKeyboardShow,
+    this.followArrow,
+    this.distanceBetweenTargetWidget,
+  });
+
+  @override
+  Color get barrierColor => Colors.black.withOpacity(0.2);
+
+  @override
+  String get barrierLabel => 'CustomPageRoute';
+
+  @override
+  bool get maintainState => true;
+
+  bool get isCenterDialog =>
+      dialogType == DialogType.center ||
+      dialogType == DialogType.adaptiveCenter;
+
+  late DeviceType deviceType;
+
+  @override
+  Duration get transitionDuration => Duration(milliseconds: 300);
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    deviceType = getDeviceType(context);
+
+    Widget modalBottomSheet = SafeArea(
+      child: CustomModalBottomSheet(
+        enableDrag: false,
+        route: this,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        clipBehavior: Clip.antiAlias,
+      ),
+    );
+
+    double centerDialogHeight = MediaQuery.of(context).size.height * 0.8;
+    double centerDialogWidth = MediaQuery.of(context).size.width * 0.8;
+
+    Widget dialog = CustomDialog(
+      context: context,
+      height: height ?? (isCenterDialog ? centerDialogHeight : null),
+      width: width ?? (isCenterDialog ? centerDialogWidth : null),
+      alignTargetWidget: alignTargetWidget ?? AlignTargetWidget.right,
+      enableArrow: enableArrow ?? true,
+      targetWidgetContext: targetWidgetContext,
+      onTapOutside: onTapOutside,
+      adjustment: adjustment ?? Offset.zero,
+      showOverFlowArrow: showOverFlowArrow ?? true,
+      overflowLeft: overflowLeft ?? 0,
+      followArrow: followArrow ?? false,
+      child: builder(context),
+    );
+
+    return deviceType == DeviceType.mobile ? modalBottomSheet : dialog;
+  }
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    deviceType = getDeviceType(context);
+    if (deviceType == DeviceType.mobile || isCenterDialog) {
+      const begin = Offset(0.0, 1.0);
+      const end = Offset.zero;
+      const curve = Curves.easeInOut;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      var offsetAnimation = animation.drive(tween);
+
+      return SlideTransition(position: offsetAnimation, child: child);
+    } else {
+      final CurvedAnimation fadeAnimation = CurvedAnimation(
+        parent: animation,
+        curve: Curves.easeInOut,
+      );
+      if (animation.status == AnimationStatus.reverse) {
+        return FadeTransition(
+          opacity: fadeAnimation,
+          child: child,
+        );
+      }
+      return FadeTransition(
+        opacity: fadeAnimation,
+        child: ScaleTransition(
+          scale: animation.drive(_dialogScaleTween),
+          child: child,
+        ),
+      );
+    }
+  }
+
+  final Animatable<double> _dialogScaleTween =
+      Tween<double>(begin: 1.3, end: 1.0)
+          .chain(CurveTween(curve: Curves.linearToEaseOut));
+
+  @override
+  bool get barrierDismissible => false;
+}
